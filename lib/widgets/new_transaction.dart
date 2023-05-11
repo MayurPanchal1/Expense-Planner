228c1ed70fb8a65f29a2d0511bf37faba44f5cfc
Mayur Panchal <mayurpanchal527@gmail.com>
2023-05-11 13:52:55 +0530

    first commit

---
+++ lib/widgets/new_transaction.dart
@@ -0,0 +1,112 @@
+import 'dart:io';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_complete_guide/widgets/adaptive_flat_button.dart';
+import 'package:intl/intl.dart';
+
+class NewTransaction extends StatefulWidget {
+  final Function addTx;
+
+  NewTransaction(this.addTx);
+
+  @override
+  State<NewTransaction> createState() => _NewTransactionState();
+}
+
+class _NewTransactionState extends State<NewTransaction> {
+  DateTime _selectedDate;
+
+  void _submitData() {
+    if (_amountController.text.isEmpty) {
+      return;
+    }
+    final enteredTitle = _titleController.text;
+    final enteredAmount = double.parse(_amountController.text);
+
+    if (enteredTitle.isEmpty || enteredAmount <= 0 || _selectedDate == null) {
+      return;
+    }
+
+    widget.addTx(
+      enteredTitle,
+      enteredAmount,
+      _selectedDate,
+    );
+    Navigator.of(context).pop();
+  }
+
+  void _presentDatePicker() {
+    showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(2020),
+      lastDate: DateTime.now(),
+    ).then((pickedDate) {
+      if (pickedDate == null) {
+        return;
+      }
+      setState(() {
+        _selectedDate = pickedDate;
+      });
+    });
+  }
+
+  final _titleController = TextEditingController();
+  final _amountController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Card(
+        elevation: 5,
+        child: Container(
+          padding: EdgeInsets.only(
+            left: 10,
+            right: 10,
+            top: 10,
+            bottom: MediaQuery.of(context).viewInsets.bottom + 10,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: <Widget>[
+              TextField(
+                decoration: InputDecoration(labelText: 'Title'),
+                controller: _titleController,
+                onSubmitted: (_) => _submitData(),
+              ),
+              TextField(
+                decoration: InputDecoration(labelText: 'Amount'),
+                controller: _amountController,
+                keyboardType: TextInputType.number,
+                onSubmitted: (_) => _submitData(),
+              ),
+              Container(
+                height: 60,
+                child: Row(
+                  children: <Widget>[
+                    Text(
+                      _selectedDate == null
+                          ? 'No date chosen'
+                          : DateFormat.yMd().format(_selectedDate),
+                    ),
+                    AdaptiveFlatButton('Choose date', _presentDatePicker),
+                  ],
+                ),
+              ),
+              // RaisedButton(
+              //   onPressed: _submitData,
+              //   child: Text('Add Transaction'),
+              //   textColor: Theme.of(context).textTheme.button.color,
+              //   color: Theme.of(context).primaryColor,
+              // )
+              ElevatedButton(
+                onPressed: _submitData,
+                child: Text('Add Transaction'),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
